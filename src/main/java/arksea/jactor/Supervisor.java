0188d06d648f2a6a3ddb12456103d9f3fc5968eb
xiaohaixing <xiaohaixing@91.com>
2014-11-12 18:36:04 +0800

    bug：创建任务时的线程安全问题

--- src/main/java/arksea/jactor/Supervisor.java
+++ src/main/java/arksea/jactor/Supervisor.java
@@ -1,8 +1,8 @@
 package arksea.jactor;
 
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import org.slf4j.Logger;
@@ -10,38 +10,51 @@ import org.slf4j.LoggerFactory;
 
 /**
  * 线程监控者，当线程ChildTask异常退出时负责将其重启
- * $Author: arksea $
+ * Author: arksea
  */
 public class Supervisor implements Thread.UncaughtExceptionHandler {
 
+    public final String name;
+    
     private final Logger logger = LoggerFactory.getLogger(Supervisor.class.getName());
     private final RestartStrategies restartStrategies;
     private final Map<String, ChildTask> childMap = new HashMap();
-    private final List<ChildInfo> childList = new LinkedList();
+    private final List<ChildInfo> childList; //保留子任务启动顺序
     private final long JOIN_CHILD_TIMEOUT = 5000;
-    private final String regname;
     private long restartCount = 0;
     private long lastRestartTime = 0;
 
+    public ChildTask getChild(String childName) {
+        return childMap.get(childName);
+    }
+    
     protected Supervisor(String name, RestartStrategies restart, ChildInfo[] childs) {
-        logger.trace("Supervisor '" + name + "' starting childs");
-        this.regname = name;
-        this.restartStrategies = restart;
-        for (ChildInfo c : childs) {
-            this.childList.add(c);
+        synchronized(this) {
+            logger.trace("Supervisor '" + name + "' starting childs");
+            this.name = name;
+            this.restartStrategies = restart;
+            childList = java.util.Collections.unmodifiableList(Arrays.asList(childs));
+            startAll();
+            logger.trace("Supervisor '" + name + "' started all childs");
         }
-        startAll();
-        logger.trace("Supervisor '" + name + "' started all childs");
     }
 
-    protected void stopAll() {
-        logger.trace("Supervisor '" + regname + "' stopping childs");
-        for (ChildInfo i : childList) {
-            ChildTask t = childMap.get(i.getName());
+    protected synchronized void stopAll() {
+        logger.trace("Supervisor '" + name + "' stopping childs");
+        for (int i = childList.size()-1; i>=0; --i) {
+            ChildInfo info = childList.get(i);
+            ChildTask t = childMap.get(info.name);
             t.normalStopRequest();
-            TaskContext.instance().remove(i.getName());
+            TaskContext.instance().remove(info.name);
+            try {
+                t.join(JOIN_CHILD_TIMEOUT);
+            } catch (InterruptedException ex) {
+            }
+            if (t.isAlive()) {
+                t.stop();//超时则强制退出线程
+            }
         }
-        logger.trace("Supervisor '" + regname + "' stopped all childs");
+        logger.trace("Supervisor '" + name + "' stopped all childs");
     }
 
     private void startAll() {
@@ -49,7 +62,7 @@ public class Supervisor implements Thread.UncaughtExceptionHandler {
             createChild(c);
         }
         for (ChildInfo c : childList) {
-            Thread child = childMap.get(c.getName());
+            Thread child = childMap.get(c.name);
             child.start();
         }
     }
@@ -57,57 +70,57 @@ public class Supervisor implements Thread.UncaughtExceptionHandler {
     private Thread createChild(ChildInfo info) {
         try {
             ChildTask child;
-            if (Actor.class.isAssignableFrom(info.getClazz())) {
-                Constructor con = info.getClazz().getConstructor(String.class, long.class, info.getArgsClass());
-                child = (ChildTask) con.newInstance(info.getName(), info.getMaxQueueLen(), info.getArgs());
+            if (Actor.class.isAssignableFrom(info.clazz)) {
+                Constructor con = info.clazz.getConstructor(String.class, long.class, info.getArgsClass());
+                child = (ChildTask) con.newInstance(info.name, info.maxQueueLen, info.state);
                 child.setDaemon(info.isDaemon());
-                TaskContext.instance().put(info.getName(), (Actor) child);
+                TaskContext.instance().put(info.name, (Actor) child);
             } else {
-                Constructor con = info.getClazz().getConstructor(String.class, info.getArgsClass());
-                child = (ChildTask) con.newInstance(info.getName(), info.getArgs());
+                Constructor con = info.clazz.getConstructor(String.class, info.getArgsClass());
+                child = (ChildTask) con.newInstance(info.name, info.state);
                 child.setDaemon(info.isDaemon());
             }
             child.setUncaughtExceptionHandler(this);
-            childMap.put(info.getName(), child);
+            childMap.put(info.name, child);
             return child;
         } catch (Throwable ex) {
-            throw new RuntimeException("Supervisor '" + regname + "' create ChildTask '" + info.getName() + "' failed", ex);
+            throw new RuntimeException("Supervisor '" + name + "' create ChildTask '" + info.name + "' failed", ex);
         }
     }
 
     @Override
     public void uncaughtException(Thread t, Throwable ex) {
-        String name = t.getName();
-        logger.error("ChildTask '" + name + "' exited because Exception", ex);
-        switch (restartStrategies) {
-            case ONE_FOR_ONE:
-                stopOneForOne(name);
-                break;
-            case ONE_FOR_ALL:
-                stopOneForAll(name);
-                break;
-            case REST_FOR_ONE:
-                stopRestForOne(name);
-                break;
-        }
+        String childName = t.getName();
+        logger.error("ChildTask '" + childName + "' exited because Exception", ex);
         ++restartCount;
         long now = System.currentTimeMillis();
         long time = now - lastRestartTime;
         lastRestartTime = now;
         if (time < 10000) {
             if (restartCount > 10) {
-                logger.error("ChileTask异常重启次数短时间内超过10次，将被停止："+regname);
-                TaskContext.instance().stop(regname);
+                logger.error("ChileTask异常重启次数短时间内超过10次，将被停止："+name);
+                TaskContext.instance().stop(name);
             }
         } else {
             restartCount = 1;
+            switch (restartStrategies) {
+                case ONE_FOR_ONE:
+                    stopOneForOne(childName);
+                    break;
+                case ONE_FOR_ALL:
+                    stopOneForAll(childName);
+                    break;
+                case REST_FOR_ONE:
+                    stopRestForOne(childName);
+                    break;
+            }
         }
     }
 
-    private void stopOneForOne(String name) {
+    private void stopOneForOne(String childName) {
         for (ChildInfo i : childList) {
-            if (i.getName().equals(name)) {
-                TaskContext.instance().remove(name);
+            if (i.name.equals(childName)) {
+                TaskContext.instance().remove(childName);
                 Thread child = createChild(i);
                 child.start();
                 break;
@@ -115,19 +128,19 @@ public class Supervisor implements Thread.UncaughtExceptionHandler {
         }
     }
 
-    private void stopOneForAll(String name) {
+    private void stopOneForAll(String childName) {
         for (ChildInfo i : childList) {
-            TaskContext.instance().remove(name);
-            ChildTask child = childMap.get(i.getName());
+            TaskContext.instance().remove(childName);
+            ChildTask child = childMap.get(i.name);
             child.normalStopRequest();
             try {
                 child.join(JOIN_CHILD_TIMEOUT);
             } catch (InterruptedException ex) {
-                logger.warn("Supervisor '" + regname + "' join ChildTask '" + name + "' interruped", ex);
+                logger.warn("Supervisor '" + name + "' join ChildTask '" + childName + "' interruped", ex);
             }
             if (child.isAlive()) {
                 child.stop();//超时则强制退出线程
-                logger.warn("Supervisor '" + regname + "' join ChildTask '" + name + "' timeout");
+                logger.warn("Supervisor '" + name + "' join ChildTask '" + childName + "' timeout");
             }
         }
         for (ChildInfo i : childList) {
@@ -136,22 +149,22 @@ public class Supervisor implements Thread.UncaughtExceptionHandler {
         }
     }
 
-    private void stopRestForOne(String name) {
+    private void stopRestForOne(String childName) {
         boolean rest = false;
         for (ChildInfo i : childList) {
-            if (rest || i.getName().equals(name)) {
+            if (rest || i.name.equals(childName)) {
                 if (rest) {
-                    TaskContext.instance().remove(name);
-                    ChildTask child = childMap.get(i.getName());
+                    TaskContext.instance().remove(childName);
+                    ChildTask child = childMap.get(i.name);
                     child.normalStopRequest();
                     try {
                         child.join(JOIN_CHILD_TIMEOUT);
                     } catch (InterruptedException ex) {
-                        logger.warn("Supervisor '" + regname + "' join ChildTask '" + name + "' interruped", ex);
+                        logger.warn("Supervisor '" + name + "' join ChildTask '" + childName + "' interruped", ex);
                     }
                     if (child.isAlive()) {
                         child.stop();//超时则强制退出线程
-                        logger.warn("Supervisor '" + regname + "' join ChildTask '" + name + "' timeout");
+                        logger.warn("Supervisor '" + name + "' join ChildTask '" + childName + "' timeout");
                     }
                 }
                 rest = true;
@@ -159,7 +172,7 @@ public class Supervisor implements Thread.UncaughtExceptionHandler {
         }
         rest = false;
         for (ChildInfo i : childList) {
-            if (rest || i.getName().equals(name)) {
+            if (rest || i.name.equals(childName)) {
                 Thread child = createChild(i);
                 child.start();
                 rest = true;
--- src/main/java/arksea/jactor/TaskContext.java
+++ src/main/java/arksea/jactor/TaskContext.java
@@ -1,9 +1,11 @@
 package arksea.jactor;
 
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
@@ -11,31 +13,42 @@ import java.util.TimerTask;
  */
 public class TaskContext {
 
-    private static TaskContext context = new TaskContext();
-    private final Map<String, Actor> serviceMap = new HashMap();
-    private final Map<String, Supervisor> supMap = new HashMap();
-    private Timer timer = new Timer("TaskContext Timer", true);
+    private static final TaskContext context = new TaskContext();
+    private final Map<String, Actor> serviceMap = new ConcurrentHashMap();
+    private final List<Supervisor> supList = new LinkedList();
+    private final Timer timer = new Timer("TaskContext Timer", true);
 
     public static TaskContext instance() {
         return context;
     }
 
-    public void start(String supervisorName, RestartStrategies restart, ChildInfo[] childs) {
+    public synchronized Supervisor start(String supervisorName, RestartStrategies restart, ChildInfo[] childs) {
         Supervisor sup = new Supervisor(supervisorName, restart, childs);
-        supMap.put(supervisorName, sup);
+        supList.add(sup);
+        return sup;
+    }
+    
+    public synchronized Supervisor start(RestartStrategies restart, ChildInfo child) {
+        Supervisor sup = new Supervisor(child.name, restart, new ChildInfo[]{child});
+        supList.add(sup);
+        return sup;
     }
 
     public synchronized void stopAll() {
-        for (Supervisor s : supMap.values()) {
+        for (int i = supList.size()-1; i>=0; --i) {
+            Supervisor s = supList.get(i);
             s.stopAll();
         }
-        supMap.clear();
+        supList.clear();
     }
 
-    public void stop(String supervisorName) {
-        Supervisor sup = supMap.remove(supervisorName);
-        if (sup != null) {
-            sup.stopAll();
+    public synchronized void stop(String supervisorName) {
+        for (Supervisor s: supList) {
+            if (s.name.equals(supervisorName)) {
+                s.stopAll();
+                supList.remove(s);
+                break;
+            }
         }
     }
 
@@ -43,31 +56,23 @@ public class TaskContext {
     }
 
     protected void put(String name, Actor svc) {
-        synchronized (serviceMap) {
-            //不能直接替换子任务，否则被替换者将会成为一个“悬挂”的同名子任务
-            if (serviceMap.get(name) != null) {
-                throw new RuntimeException("register a ChildTask failed,The name '" + name + "' is exist");
-            }
-            serviceMap.put(name, svc);
+        //不能直接替换子任务，否则被替换者将会成为一个“悬挂”的同名子任务
+        if (serviceMap.get(name) != null) {
+            throw new RuntimeException("register a ChildTask failed,The name '" + name + "' is exist");
         }
+        serviceMap.put(name, svc);
     }
 
     protected void remove(String name) {
-        synchronized (serviceMap) {
-            serviceMap.remove(name);
-        }
+        serviceMap.remove(name);
     }
 
     protected Actor get(String name) {
-        synchronized (serviceMap) {
-            return serviceMap.get(name);
-        }
+        return serviceMap.get(name);
     }
 
     public boolean exist(String name) {
-        synchronized (serviceMap) {
-            return serviceMap.containsKey(name);
-        }
+        return serviceMap.containsKey(name);
     }
 
     //发送消息给目标任务，这种调用不会阻塞调用者，也不会阻塞被调用者
@@ -79,9 +84,14 @@ public class TaskContext {
             throw new TaskNotExistException(dest);
         }
     }
+// 直接向Task引用发送消息，会在Task被重启后发给一个已经死掉的Task
+//    public void send(MessageChildTask svc, Message msg) {
+//        if (svc != null) {
+//            svc.putInfo(msg);
+//        }
+//    }
     //延迟发送消息给目标任务，这种调用不会阻塞调用者，也不会阻塞被调用者，
     //注意：对于向自身投递消息的Task，如果延时期间失败被重启，消息不会被送到新的任务线程中
-
     public void send_after(final long milliseconds, final String dest, final Message msg) {
         final Actor svc = get(dest);
         final String source = Thread.currentThread().getName();
@@ -110,51 +120,64 @@ public class TaskContext {
             throw new TaskNotExistException(dest);
         }
     }
+//    public void send_after(final long milliseconds, final MessageChildTask svc, final Message msg) {
+//        assert (svc!=null);
+//        TimerTask task = new TimerTask() {
+//            @Override
+//            public void run() {
+//                svc.putInfo(msg);
+//            }
+//        };
+//        timer.schedule(task, milliseconds);
+//    }
     //发送消息给目标任务，要求其处理请求后返回处理结果，这种调用会阻塞调用者，也会阻塞被调用者
-
     public Message call(String dest, Message callmsg, long timeout) {
-        assert (!dest.equals(Thread.currentThread().getName()));
-        if (timeout == 0) {
-            throw new RuntimeException("timeout must not be zero.");
-        }
         final Actor svc = get(dest);
-        if (svc != null) {
-            //每次都创建新的Result实例，并在其上wait/notify，防止错误的信号通知
-            Result result = new Result();
-            synchronized (result) {
-                svc.putCall(callmsg, result);
-                try {
-                    result.wait(timeout);
-                } catch (InterruptedException ex) {
-                    throw new TaskInterruptedException("call interrupted  ***  method=" + callmsg.name +
-                            ",caller=" + Thread.currentThread().getName() +
-                            ",dest=" + dest, ex);
-                }
-            }
-            if (result.msg != null && (result.msg instanceof ThrowMessage)) {
-                Object v = result.msg.value;
-                if (v instanceof Throwable) {
-                    throw new TaskCallException("  ***  method=" + callmsg.name +
-                            ",caller=" + Thread.currentThread().getName() +
-                            ",dest=" + dest, (Throwable) v);
-                } else {
-                    throw new TaskCallException((String) v + "  ***  method=" + callmsg.name +
-                            ",caller=" + Thread.currentThread().getName() +
-                            ",dest=" + dest);
-                }
-            } else {
-                if (!result.resulted) {
-                    throw new TaskCallException("call timeout  ***  method=" + callmsg.name +
-                            ",caller=" + Thread.currentThread().getName() +
-                            ",dest=" + dest);
-                } else {
-                    return result.msg;
-                }
-            }
-        } else {
+        if (svc == null) {
             throw new TaskNotExistException("task not exist  ***  method=" + callmsg.name +
                     ",caller=" + Thread.currentThread().getName() +
                     ",dest=" + dest);
+        }
+        return call(svc, callmsg, timeout);
+    }
+    private Message call(Actor svc, Message callmsg, long timeout) {
+        //不得从任务线程本身call自己，防止死锁
+        assert (svc!=null && !svc.getName().equals(Thread.currentThread().getName()));
+        String dest = svc.getName();
+        if (timeout == 0) {
+            throw new RuntimeException("timeout must not be zero.");
+        }
+        //每次都创建新的Result实例，并在其上wait/notify，防止错误的信号通知
+        Result result = new Result();
+        synchronized (result) {
+            svc.putCall(callmsg, result);
+            try {
+                result.wait(timeout);
+            } catch (InterruptedException ex) {
+                throw new TaskInterruptedException("call interrupted  ***  method=" + callmsg.name +
+                        ",caller=" + Thread.currentThread().getName() +
+                        ",dest=" + dest, ex);
+            }
+        }
+        if (result.msg != null && (result.msg instanceof ThrowMessage)) {
+            Object v = result.msg.value;
+            if (v instanceof Throwable) {
+                throw new TaskCallException("  ***  method=" + callmsg.name +
+                        ",caller=" + Thread.currentThread().getName() +
+                        ",dest=" + dest, (Throwable) v);
+            } else {
+                throw new TaskCallException((String) v + "  ***  method=" + callmsg.name +
+                        ",caller=" + Thread.currentThread().getName() +
+                        ",dest=" + dest);
+            }
+        } else {
+            if (!result.resulted) {
+                throw new TaskCallException("call timeout  ***  method=" + callmsg.name +
+                        ",caller=" + Thread.currentThread().getName() +
+                        ",dest=" + dest);
+            } else {
+                return result.msg;
+            }
         }
     }
 }
