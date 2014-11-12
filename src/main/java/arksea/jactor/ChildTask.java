9147f0f496900cb1280e90f13c90436eee0a5868
xiaohaixing <xiaohaixing@91.com>
2014-11-12 18:34:45 +0800

    Actor状态参数改名args-->states

--- src/main/java/arksea/jactor/ChildTask.java
+++ src/main/java/arksea/jactor/ChildTask.java
@@ -3,24 +3,19 @@ package arksea.jactor;
 /**
  * 线程监控子任务类，继承自Thread，由Supervisor启动并注册其为UncaughtExceptionHandler，
  * 当子任务线程异常退出时将会被Supervisor重启。
- * $Author: arksea $
- * @param <T>
+ * Author: arksea
  */
 public abstract class ChildTask<T> extends Thread {
 
-    private final T args;
+    protected final T state;
     /*
      * 构造函数增加子任务用于初始化的参数，
      * 当异常退出被重启时将由Supervisor重新创建并传入
      */
 
-    public ChildTask(String name, T args) {
+    public ChildTask(String name, T state) {
         super(name);
-        this.args = args;
-    }
-
-    public T getArgs() {
-        return args;
+        this.state = state;
     }
 
     public abstract void normalStopRequest();
